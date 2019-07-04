2a65870d4d189c9bbd9f97fc4185c4e0adc8d965
Gustavo Guimarães da Silva <gustavo.guimaraes.da.silva@gmail.com>
2019-07-04 16:41:47 -0300

    Adicionar as dependencias do Heroku ao arquivo requirements.txt

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn <403-viloes>.wsgi
--- viloes/settings.py
+++ viloes/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
+
+import django_heroku
+django_heroku.settings(locals())
