2ff74cc47fcb1d1d03de546cbb612b72db8beadb
Gustavo Guimarães da Silva <gustavo.guimaraes.da.silva@gmail.com>
2019-07-04 15:32:46 -0300

    Adcionar a pagina das Vilâs

--- catalogo_viloes/views.py
+++ catalogo_viloes/views.py
@@ -6,3 +6,7 @@ from catalogo_viloes.models import Viloes
 def mostrar_index(request):
     viloes = Viloes.objects.all()
     return render(request, 'index.html', {'viloes': viloes})
+
+def mostrar_vilas(request):
+    vilas = Viloes.objects.filter(categoria='Mana')
+    return render(request, 'manas.html',  {'vilas': vilas})
